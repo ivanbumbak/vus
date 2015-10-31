f924969994f68a8c7d5f993156b14ade10799c83
ivanbumbak <bumbak.ivan@gmail.com>
2015-10-31 11:40:00 +0100

    Njie bitno

--- sections/home/hero-home.php
+++ sections/home/hero-home.php
@@ -10,37 +10,44 @@
 
 		<div class="half-section subsection">
 			<header class="header-subsection">
-				<h2>Header Subsection</h2>
+				<h2>Header Half Subsection</h2>
 			</header>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero cum iusto doloribus officiis delectus alias consectetur ullam, animi totam assumenda, ad earum quis non nostrum placeat provident dolores ratione similique! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam enim dolor cum rem magnam voluptatem atque non voluptatibus maxime aliquid quasi vel, distinctio ut aliquam, quo eaque! Vel, tempora, debitis!</p>
 		</div>
 		<div class="half-section subsection">
 			<header class="header-subsection">
-				<h2>Header Subsection</h2>
+				<h2>Header Half Subsection</h2>
 			</header>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum a, distinctio. Est natus animi voluptatibus aut asperiores vitae, tenetur, optio distinctio! Obcaecati cumque tempora esse soluta ipsam debitis atque enim? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit eveniet debitis ea pariatur ducimus corrupti et nisi vel in, ipsam nemo quo eaque nesciunt culpa laborum. Aut delectus blanditiis dolores.</p>
 		</div>
 
 		<div class="third-section subsection">
+			<h2>Header Third Subsection</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, impedit explicabo, sed, nemo aliquid ratione voluptates hic eos, error temporibus quibusdam. Eligendi distinctio ipsam esse optio fugit, quaerat neque fugiat!</p>
 		</div>
 		<div class="third-section subsection">
+			<h2>Header Third Subsection</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore laudantium quis, corporis vel, blanditiis quibusdam! Quasi cum quam vero molestiae, fugiat ullam eum ad vitae, quisquam officia necessitatibus nam distinctio.</p>
 		</div>
 		<div class="third-section subsection">
+			<h2>Header Third Subsection</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, cupiditate ratione sint, veritatis amet dolore consectetur dolor in iste assumenda, quas eligendi. Perferendis et cumque a alias repellendus, ipsam consequuntur.</p>
 		</div>
 
 		<div class="quarter-section subsection">
+			<h2>Header Quarter Subsection</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum modi dignissimos accusamus atque, consectetur aliquam veniam nobis voluptates, dolorum aperiam, sit ea illo ipsum voluptatum cum. Fugiat saepe doloremque, quo!</p>
 		</div>
 		<div class="quarter-section subsection">
+			<h2>Header Quarter Subsection</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos laborum mollitia, rerum expedita atque esse impedit! Excepturi architecto mollitia, sequi odio aperiam hic dolorem, quisquam doloremque eius, vitae cupiditate eos.</p>
 		</div>
 		<div class="quarter-section subsection">
+			<h2>Header Quarter Subsection</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque natus molestiae illum, nesciunt molestias debitis blanditiis repudiandae quaerat cupiditate, iure dolores accusamus animi tempora quia laborum ex quibusdam labore saepe.</p>
 		</div>
 		<div class="quarter-section subsection">
+			<h2>Header Quarter Subsection</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus natus modi eligendi sunt nam atque aut obcaecati facere ullam qui doloremque, incidunt alias, in neque nemo! Ipsam labore, molestiae maiores.</p>
 		</div>
 	</div>
