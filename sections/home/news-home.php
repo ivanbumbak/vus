6d0a0b185a1905af6021722565e10a59e1a471ce
Krešimir Čoko <kresimircoko@gmail.com>
2015-12-08 16:29:29 +0100

    News Home Section
    Posts are output based on their category, 2 most recent posts per
    category, did the basic design

--- sections/home/news-home.php
+++ sections/home/news-home.php
@@ -1,54 +1,57 @@
+<?php
+// TODO: Kategorije
+// TODO: Vrijeme
+// TODO: Popravit Excerpt
+// TODO: Ponašanje slike
+?>
 <section class="news-home main-section">
 	<div class="container">
 		<div class="section-heading">
 			<h1>Hero Home Section</h1>
 		</div>
 
-		<div class="full-subsection subsection">
-			<h1>Full Subsection</h1>
+		<div id="home_news-container" class="half-section">
+			<!-- Ispisuje dvi najnovije obavijesti s kategorijom 'news' -->
+			<?php
+				$newsCategoryID = 2; //the certain category ID
+				$latestPosts = new WP_Query(
+				array('posts_per_page' => 2, 'category__in' => array($newsCategoryID)));
+
+				if($latestPosts->have_posts()) :
+					while($latestPosts->have_posts()) :
+						$latestPosts->the_post();
+			?>
+			<!-- SAV sadržaj i informacije od objave idu ode -->
+					<div class="home-post full-section subsection">
+						<a class="home-post_title href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a>
+						<?php the_excerpt(); ?>
+					</div>
+			<?php
+					endwhile;
+				endif;
+			?>
 		</div>
 
-		<div class="half-section subsection">
-			<header class="subsection-header">
-				<h2>Header Half Subsection</h2>
-			</header>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero cum iusto doloribus officiis delectus alias consectetur ullam, animi totam assumenda, ad earum quis non nostrum placeat provident dolores ratione similique! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam enim dolor cum rem magnam voluptatem atque non voluptatibus maxime aliquid quasi vel, distinctio ut aliquam, quo eaque! Vel, tempora, debitis!</p>
-		</div>
-		<div class="half-section subsection">
-			<header class="subsection-header">
-				<h2>Header Half Subsection</h2>
-			</header>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum a, distinctio. Est natus animi voluptatibus aut asperiores vitae, tenetur, optio distinctio! Obcaecati cumque tempora esse soluta ipsam debitis atque enim? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit eveniet debitis ea pariatur ducimus corrupti et nisi vel in, ipsam nemo quo eaque nesciunt culpa laborum. Aut delectus blanditiis dolores.</p>
-		</div>
+		<div id="home_events-container" class="half-section">
+			<!-- Ispisuje dvi najnovije obavijesti s kategorijom 'events' -->
+			<?php
+				$eventsCategoryID = 3; //the certain category ID
+				$latestPosts = new WP_Query(
+				array('posts_per_page' => 2, 'category__in' => array($eventsCategoryID)));
 
-		<div class="third-section subsection">
-			<h2>Header Third Subsection</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, impedit explicabo, sed, nemo aliquid ratione voluptates hic eos, error temporibus quibusdam. Eligendi distinctio ipsam esse optio fugit, quaerat neque fugiat!</p>
-		</div>
-		<div class="third-section subsection">
-			<h2>Header Third Subsection</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore laudantium quis, corporis vel, blanditiis quibusdam! Quasi cum quam vero molestiae, fugiat ullam eum ad vitae, quisquam officia necessitatibus nam distinctio.</p>
-		</div>
-		<div class="third-section subsection">
-			<h2>Header Third Subsection</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, cupiditate ratione sint, veritatis amet dolore consectetur dolor in iste assumenda, quas eligendi. Perferendis et cumque a alias repellendus, ipsam consequuntur.</p>
-		</div>
-
-		<div class="quarter-section subsection">
-			<h2>Header Quarter Subsection</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum modi dignissimos accusamus atque, consectetur aliquam veniam nobis voluptates, dolorum aperiam, sit ea illo ipsum voluptatum cum. Fugiat saepe doloremque, quo!</p>
-		</div>
-		<div class="quarter-section subsection">
-			<h2>Header Quarter Subsection</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos laborum mollitia, rerum expedita atque esse impedit! Excepturi architecto mollitia, sequi odio aperiam hic dolorem, quisquam doloremque eius, vitae cupiditate eos.</p>
-		</div>
-		<div class="quarter-section subsection">
-			<h2>Header Quarter Subsection</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque natus molestiae illum, nesciunt molestias debitis blanditiis repudiandae quaerat cupiditate, iure dolores accusamus animi tempora quia laborum ex quibusdam labore saepe.</p>
-		</div>
-		<div class="quarter-section subsection">
-			<h2>Header Quarter Subsection</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus natus modi eligendi sunt nam atque aut obcaecati facere ullam qui doloremque, incidunt alias, in neque nemo! Ipsam labore, molestiae maiores.</p>
+				if($latestPosts->have_posts()) :
+					while($latestPosts->have_posts()) :
+						$latestPosts->the_post();
+			?>
+			<!-- SAV sadržaj i informacije od objave idu ode -->
+					<div class="home-post full-section subsection">
+						<a class="home-post_title href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a>
+						<?php the_excerpt(); ?>
+					</div>
+			<?php
+					endwhile;
+				endif;
+			?>
 		</div>
 	</div>
 
