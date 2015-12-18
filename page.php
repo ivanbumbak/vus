9c6027ce4a8086850183e09ebd1a7a3bc0fd47c8
Krešimir Čoko <kresimircoko@gmail.com>
2015-12-19 00:45:33 +0100

    Added Page Content

--- page.php
+++ page.php
@@ -14,10 +14,16 @@
 
 get_header(); ?>
 
-	<main id="main" class="site-main" role="main">
-
-
-	</main><!-- #main -->
+<section class="page-section main-section">
+	<div class="container">
+		<main id="main" class="site-main" role="main">
+			<?php while(have_posts()) : the_post(); ?>
+				<?php get_template_part('template-parts/content', 'page'); ?>
+				<?php // the_post_navigation(); ?>
+			<?php endwhile; // End of the loop. ?>
+		</main><!-- #main -->
+	</div>
+</section>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
